ad5310ad0d40e3ef6a83fc09aee4703582b45816
User Maximum <maximum@d1.maximum.cc>
2015-03-12 21:26:42 +0800

    Add get days of month function, and automatially download

--- browser.py
+++ browser.py
@@ -4,6 +4,8 @@ import re
 import mechanize
 from bs4 import BeautifulSoup
 import calendar
+import json
+import os
 
 
 info = []
@@ -22,6 +24,15 @@ for year in range(begin, end + 1):
 		days = calendar.monthrange(year, month)[1]
 
 		for day in range(1, days + 1):
+			
+			""" Create folder and file for storing data """
+			c_path = os.getcwd()
+			directory = c_path + '/' +str(year)
+			if not os.path.exists(directory):
+				print "create directory"
+				os.makedirs(directory)
+			filename = str(month) + '-' + str(day) + '.json'
+			f = open(directory + '/' + filename, 'w+')
 
 			""" Generate data for post form """
 			r = br.open(URL)
@@ -68,9 +79,9 @@ for year in range(begin, end + 1):
 				"now_percent": m.group(11)
 						}
 					info.append(content)
-
-					print m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6), m.group(7), m.group(8), m.group(9), m.group(10), m.group(11)
-	
+					f.write(json.dumps(content, ensure_ascii=False))
+					#print m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6), m.group(7), m.group(8), m.group(9), m.group(10), m.group(11)
+			f.close	
 			#print info[1]
 
 
